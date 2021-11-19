f1770733052cba4a0cf787a58b145ce3b730a2ad
Matthew Lese <matthewjlese@gmail.com>
2021-11-19 14:09:22 -0800

    completes basic setup

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Route, Redirect, Routes, Link, HashRouter } from 'react-router-dom'
+
+const App = () => (
+  <div id='app'>
+    App Component
+  </div>
+)
+
+export default App
