febc699461e40be0520732078afcb7f932bc6730
Matthew Lese <matthewjlese@gmail.com>
2021-11-19 15:24:03 -0800

    completes appcon

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ApplicationRecord
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  def require_session_token
+  def reset_session_token!
     self.session_token = generate_session_token
     self.save!
     self.session_token
