0403da70dfcb45ce8ed2401f7b9c175c3dd6e852
Matthew Lese <matthewjlese@gmail.com>
2021-11-23 14:59:15 -0800

    story seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,9 @@
 
 # demo user seed
 demo = User.create([{ email: 'demo@demo.demo', password: 'qwerty', name: 'Demo User'}])
+# dummy users
+users = User.create([{ email: 'steve@demo.demo', password: '111111', name: 'Steve'},
+{ email: 'jeff@demo.demo', password: '111111', name: 'Jeff'},
+{ email: 'suzie@demo.demo', password: '111111', name: 'Suzie'}])
+
+stories = Story.create([{ title: 'How to test that your stories are working', body: 'seed some data, firstly', author_id: 3}])
