61a6bb53c1c9a47011d31daa77b3303de39d8cfe
Matthew Lese <matthewjlese@gmail.com>
2021-11-23 16:43:03 -0800

    stories reducer completed

--- frontend/reducers/stories_reducer.js
+++ frontend/reducers/stories_reducer.js
@@ -1,11 +1,16 @@
 import { RECEIVE_STORIES, RECEIVE_STORY, REMOVE_STORY } from "../actions/story_actions";
 
 export default (state={}, action) => {
+  Object.freeze(state)
+  let nextState = Object.assign({}, state)
   switch (action.type) {
-    case value:
-      
-      break;
-  
+    case RECEIVE_STORIES:
+      return action.stories
+    case RECEIVE_STORY:
+      return Object.assign(nextState, { [action.story.id]: action.story })
+    case REMOVE_STORY:
+      delete nextState[action.storyId]
+      return nextState
     default:
       break;
   }
