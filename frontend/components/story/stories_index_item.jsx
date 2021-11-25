09f8cd33d017b8b0207a56803d8250900ae14877
Matthew Lese <matthewjlese@gmail.com>
2021-11-24 16:47:21 -0800

    changes to class component

--- frontend/components/story/stories_index_item.jsx
+++ frontend/components/story/stories_index_item.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-const StoriesIndexItem = props => {
-  
-  return(
-    <li className='story-list-item'>
-      <h3 className='story-item-title'>{props.story.title}</h3>
-    </li>
-  )
+// const StoriesIndexItem = props => {
+class StoriesIndexItem extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return(
+      <li className='story-list-item'>
+        <h3 className='story-item-title'>{this.props.story.title}</h3>
+      </li>
+    )
+  }
 }
 
 export default StoriesIndexItem
