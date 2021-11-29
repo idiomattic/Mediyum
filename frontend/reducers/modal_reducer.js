236ec4cae790d2b581e065e55fec7d33d8ed6d10
Matthew Lese <matthewjlese@gmail.com>
2021-11-29 11:23:23 -0800

    conforms with latest heroku push

--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -1,9 +1,10 @@
+import { RECEIVE_COMMENT, REMOVE_COMMENT } from "../actions/comment_actions";
 import { DISPLAY_MODAL, HIDE_MODAL } from "../actions/modal_actions";
 
 export default (state=null, action) => {
   switch (action.type) {
     case DISPLAY_MODAL:
-      return action.modal 
+      return action.modal
     case HIDE_MODAL:
       return null
     default:
