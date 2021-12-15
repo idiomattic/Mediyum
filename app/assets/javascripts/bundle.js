9fc253abc41d83b079563366134ecd876cb2d479
Matthew Lese <matthewjlese@gmail.com>
2021-12-15 15:04:27 -0800

    formatting

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -3434,70 +3434,28 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 
+var UserNav = function UserNav(props) {
+  var currentUserId = props.currentUserId,
+      currentUser = props.currentUser,
+      displayModal = props.displayModal;
 
-
-var UserNav = /*#__PURE__*/function (_React$Component) {
-  _inherits(UserNav, _React$Component);
-
-  var _super = _createSuper(UserNav);
-
-  function UserNav(props) {
-    _classCallCheck(this, UserNav);
-
-    return _super.call(this, props);
+  if (!currentUserId) {
+    return null;
   }
 
-  _createClass(UserNav, [{
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      var _this$props = this.props,
-          currentUserId = _this$props.currentUserId,
-          currentUser = _this$props.currentUser;
-
-      if (!currentUserId) {
-        return null;
-      }
-
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
-        className: "user-nav",
-        onClick: function onClick() {
-          return _this.props.displayModal();
-        }
-      }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
-        className: "user-photo",
-        src: currentUser.photoUrl,
-        alt: "img"
-      }));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", {
+    className: "user-nav",
+    onClick: function onClick() {
+      return displayModal();
     }
-  }]);
-
-  return UserNav;
-}(react__WEBPACK_IMPORTED_MODULE_0__.Component);
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("img", {
+    className: "user-photo",
+    src: currentUser.photoUrl,
+    alt: "img"
+  }));
+};
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (UserNav);
 
--- frontend/components/user_nav/user_nav.jsx
+++ frontend/components/user_nav/user_nav.jsx
@@ -1,22 +1,15 @@
 import React from "react"
-import { useLocation } from 'react-router-dom'
 
-class UserNav extends React.Component {
-  constructor(props) {
-    super(props)
+const UserNav = props => {
+  let {currentUserId, currentUser, displayModal} = props
+  if (!currentUserId) {
+    return null
   }
-  
-  render() {
-    let {currentUserId, currentUser} = this.props
-    if (!currentUserId) {
-      return null
-    }
-    return(
-      <div className='user-nav' onClick={() => this.props.displayModal()}>
-        <img className='user-photo' src={currentUser.photoUrl} alt="img" />
-      </div>
-    )
-  }
+  return(
+    <div className='user-nav' onClick={() => displayModal()}>
+      <img className='user-photo' src={currentUser.photoUrl} alt="img" />
+    </div>
+  )
 }
 
 export default UserNav
